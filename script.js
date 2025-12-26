ff2494712d611182130edd69ec254dabc63db269
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-26 13:04:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const age = document.getElementById("age").value;
+  const name = document.getElementById("name").value;
+
+  // Validation
+  if (age === "" || name === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  // Promise logic
+  const agePromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+
+  agePromise
+    .then((message) => {
+      alert(message);
+    })
+    .catch((error) => {
+      alert(error);
+    });
+});
